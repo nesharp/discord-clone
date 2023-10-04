133020203d134d9f4a5339ead59891fe1f86b12b
nesharp <nesterukmisha27@gmail.com>
2023-10-04 07:43:45 +0300

    auth-bug-fixed

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,9 +2,8 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 export default function Home() {
     return (
-        <div className="p-2">
-            <p>Hello</p>
-            <Button variant="destructive">Click me</Button>
+        <div className="p-2 bg-red-200 text-purple-800">
+            This is a protected page
         </div>
     )
 }
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return (
+        <div>
+            <p>Some test content</p>
+        </div>
+    )
+}
+
+export default TestPage
